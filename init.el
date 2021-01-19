b83336a5a6a0a894ccd1c0459d4829b79afa768a
akshitkr <akshitkumar31102003@gmail.com>
2021-01-19 21:37:22 +0530

    setup js

--- init.el
+++ init.el
@@ -4,7 +4,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(safe-local-variable-values '((git-commit-major-mode . git-commit-elisp-text-mode))))
+ '(custom-safe-themes
+   '("e3b2bad7b781a968692759ad12cb6552bc39d7057762eefaf168dbe604ce3a4b" default))
+ '(lsp-disabled-clients '(\'js-ts))
+ '(python-shell-interpreter "python3"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
